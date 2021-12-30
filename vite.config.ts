1005812b68fb5ddd2cb75bcb311b84d05b6821da
Chirag <chirag@Chirags-MacBook-Pro.local>
2021-12-30 22:19:39 +0530

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/redux-saga-example/',
   plugins: [react()],
-  base: 'https://chirag3092.github.io/redux-saga-example/',
 });
